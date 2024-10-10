b33231efcb8336d6201be51000393fa72ba5ee62
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-10 21:51:21 +0000

    Task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""There is no module here."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return sum of list as a float."""
+    total: float = 0.0
+    for i in range(len(input_list)):
+            total += input_list[i]
+    return total
