ebf22625517e1d0f942443f77b04b5643edaa0ec
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-10 22:07:22 +0000

    Task5 v3

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""There is no module here."""
+"""There is the module for our program."""
+import typing
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """Return sum of list as a float."""
     return sum(input_list)
