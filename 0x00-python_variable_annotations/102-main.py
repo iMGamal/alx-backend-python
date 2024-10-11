c6f7d817e83c5f91c3c871b0d61a7a4be2083b62
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-11 20:51:04 +0000

    Task102

--- 0x00-python_variable_annotations/102-main.py
+++ 0x00-python_variable_annotations/102-main.py
@@ -3,6 +3,3 @@
 zoom_array =  __import__('102-type_checking').zoom_array
 
 print(zoom_array.__annotations__)
-
-bob@dylan:~$ ./102-main.py 
-{'lst': typing.Tuple, 'factor': <class 'int'>, 'return': typing.List}
