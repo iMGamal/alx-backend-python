558a6cd51224940d4690ca0fec450a2f8c502d2e
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-11 19:37:10 +0000

    Task9 v2

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This is the module for our program."""
-import typing
+from typing import List, Sequence, Tuple, Iterable
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+type_lst = Iterable[Sequence]
+def element_length(lst: type_lst) -> List[Tuple[Sequence, int]]:
     """Return length of an object."""
     return [(i, len(i)) for i in lst]
