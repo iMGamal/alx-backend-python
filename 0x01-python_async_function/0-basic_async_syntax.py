149788c317624a0acf9e6481a3ba2b378c9b950e
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-15 13:48:07 +0000

    Task 0 v2

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """There is the module for our pogram."""
-from asyncio import sleep
-from random import uniform
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """Return the float."""
-    random_delay = uniform(0, max_delay)
-    await sleep(random_delay)
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
     return random_delay
+
+
+if __name__ == "__main__":
+    asyncio.run(wait_random())
