75fe1b79c4e3b02f2e72365d2ea44184329895ef
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-15 23:51:26 +0000

    Task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This is the module for our program."""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Generate async."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
