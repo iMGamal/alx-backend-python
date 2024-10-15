347e51086a031d08202973c4f3254e08c9d82f5c
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-15 21:57:26 +0000

    Task1 v2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 """These are the modules for our program."""
+from basic_async_syntax import wait_random
 import asyncio
-from typing import List
-from 1-concurrent_coroutines import wait_random
+import typing
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Create a list of tasks by spawning wait_random n times."""
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    
-    # Use asyncio.gather to execute all tasks concurrently and return their results
-    delays = []
-    
-    for task in asyncio.as_completed(tasks):  # as_completed returns tasks in the order they are completed
-        delay = await task
-        delays.append(delay)  # Add completed delay to the list
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """Return a list of wait time."""
+    wait_list: typing.List[float] = []
+    for i in range(n):
+        temp: float = wait_random(max_delay)
+        wait_list.append(temp)
+    return sort(wait_list)
 
-    return delays
+
+if __name__ == "__main__":
+    asyncio.run(wait_n())
