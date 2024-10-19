76a194cd28cde047e5a87e89464d6be874107ce7
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-19 02:44:23 +0000

    Task2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This is the module for our program."""
+import asyncio
+import time
+import random
+
+
+wait_n = __import__('1-concurrent_coroutines.py').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Return total time."""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    finish = time.time()
+    total_time = finish - start
+    return total_time / n
